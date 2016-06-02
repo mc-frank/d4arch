7dfb15526c00babd71ff2319d884dec1a47ae30f
Sam Unwin <unwin100@gmail.com>
2016-06-02 19:39:07 +0100

    Threads now sort into folders

--- source/app.d
+++ source/app.d
@@ -1,19 +1,20 @@
 import std.stdio,
 	std.net.curl,
-	std.algorithm.iteration,
 	std.conv,
 	std.string,
+	std.file,
 	std.json;
 
 import vibe.data.json;
 
-//
+// API URLS
 string api_url = "http://a.4cdn.org";
 string reply_img_url = "http://i.4cdn.org";
-//
+
+// Default storage location
 string dir = "./thread/";
 
-
+// Main
 void main(string[] args) {
 
 	if(args.length < 2) {
@@ -24,6 +25,10 @@ void main(string[] args) {
 		writeln("Usage: d4arch [thread_id] [board]");
 		exit(-1);
 	}
+
+	// Set the picture save location to be under the default
+	// location concatenated with the thread id
+	dir = dir ~ args[1] ~ "/";
 
 	getThread(args[1], args[2]);
 
@@ -39,26 +44,34 @@ void getThread(string thread_id, string board) {
 	string json_string = to!string(contents);
   auto posts = parseJsonString(json_string);
 
+	// Loop through each reply checking if there's an image present,
+	// if so get the URL, and call getImage()
 	foreach(reply; posts["posts"]) {
 		if( reply["tim"].type() !=  Json.Type.undefined) {
 			string ext = reply["ext"].toString().removechars("\"");
 			string img_file = reply["tim"].toString() ~ ext;
 			writefln("%s: %s", reply["no"], img_file);
-			getImage(img_file, board);
+			getImage(img_file, board, thread_id);
 		}
 	}
 
 	// Save thread replies in .json file
-	string filename = thread_id ~ ".json";
+	/*
+	string filename = dir ~ thread_id ~ ".json";
 	File f = File(filename, "w+");
 	f.write(posts["posts"].toPrettyString());
 	f.close();
+	*/
 }
 
 // Download image to dir and filename
-void getImage(string filename, string board) {
+void getImage(string filename, string board, string thread_id) {
 
 	string URL = reply_img_url ~ "/" ~ board ~ "/" ~ filename;
+
+	if(!dir.exists()) {
+		mkdirRecurse(cast(char[]) dir);
+	}
 
 	filename = dir ~ filename;
 
